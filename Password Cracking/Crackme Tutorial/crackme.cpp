81a5a160c713a102747a8557456745d8dcea93ad
Impurity2139 <montgomeryt0919@students.bowiestate.edu>
2024-09-13 04:07:20 +0000

    Update crackme.cpp
    added comments

--- Password Cracking/Crackme Tutorial/crackme.cpp
+++ Password Cracking/Crackme Tutorial/crackme.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <string>
 
+//invoking std namespace for readablity
 using namespace std;
 
 
 int main(){
-    string Flag = "Flag{Cracked}";
+    //the flag you're looking for
+    string flag = "flag{Cracked}";
+    //the password to get the flag
     string key = "opensesame";
     string input;
+    
+    //user input for the password
     cout << "What is the passphrase?: ";
     cin >> input;
 
+    //simple if statement for the password
     if (input == "opensesame"){
-        cout << Flag << endl;
+        cout << flag << endl;
     } else {
         cout << "Wrong key" << endl;
     }
